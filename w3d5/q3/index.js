670fcfab911505a70a53f3a856f75ebbb74af1b6
Ajibade Eden <teejay.2005@yahoo.com>
2021-08-07 11:28:09 -0500

    corrections

--- w3d5/q3/index.js
+++ w3d5/q3/index.js
@@ -6,16 +6,53 @@ const date = new Date();
 const hour = date.getHours();
 
 app.use(express.urlencoded({extended: false}));
+app.use('/css', express.static(path.join(__dirname, 'css')));
 
+//correction
 //index
 app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, 'views', 'index.html'));
+    let css;
     if(hour >= 6){
-        app.use('/css', express.static(path.join(__dirname, 'css', 'day.css')));
+        css = 'day.css';
     } else {
-        app.use('/css', express.static(path.join(__dirname, 'css', 'night.css')));
+        css = 'night.css';
     }
+    res.status(200).send(`
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>W3D5</title>
+    <link rel="stylesheet" href="../css/${css}">
+</head>
+<body>
+    <div>
+        <form action="/result" method="post">
+            <div>
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name">
+                <label for="name">Age</label>
+                <input type="text" id="age" name="age">
+                <button type="submit">Submit Query</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
+    `);
 });
+
+// //index
+// app.get('/', (req, res) => {
+//     res.status(200).sendFile(path.join(__dirname, 'views', 'index.html'));
+//     if(hour >= 6){
+//         app.use('/css', express.static(path.join(__dirname, 'css', 'day.css')));
+//     } else {
+//         app.use('/css', express.static(path.join(__dirname, 'css', 'night.css')));
+//     }
+// });
 
 //submit form
 app.post('/result', (req, res) => {
